ae1238a16e34b050b41d38faec8b9f0869388ec3
Muhammad Arslan <Muhammad Arslan@DESKTOP-123456789>
2021-11-12 09:56:49 +0500

    added math functions to the main file

--- myMathJumit/src/math.java
+++ myMathJumit/src/math.java
@@ -1,4 +1,20 @@
 
 public class math {
+	
+	int myAdd(int a, int b) {
+		return a+b;
+	}
+	
+	int mySubtract(int a, int b) {
+		return a-b;
+	}
+	
+	int myMultiply(int a, int b) {
+		return a*b;
+	}
+	
+	int myDivide(int a, int b) {
+		return a/b;
+	}
 
 }
