23c60141c28f5a7cc4798a47127be0e422094b12
Muhammad Arslan <Muhammad Arslan@DESKTOP-123456789>
2021-11-12 11:21:34 +0500

    changed the test case function name to testAdd

--- 1/src/test/java/math/addTestCase1.java
+++ 1/src/test/java/math/addTestCase1.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 public class addTestCase1 {
 
 	@Test
-	public void test() {
-		test addition = new test();
+	public void testAdd() {
+		MyMathTest addition = new MyMathTest();
 		int actual = addition.myadd(10, 35);
 		int expected = 45;
 		assertEquals(expected,actual);
